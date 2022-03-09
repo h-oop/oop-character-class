08558f9979a6d946f4bf5d608a75d56c0efb73ce
h-oop <99036325+h-oop@users.noreply.github.com>
2022-03-09 09:11:33 -0700

    h

---
+++ script.js
@@ -0,0 +1,34 @@
+//define
+class character {
+    constructor(name, phrase1, phrase2){
+      this.name = name;
+      this.phrase1 = phrase1;
+      this.phrase2 = phrase2;
+      this.level = 0;
+    }
+    
+    speak(phraseNum) {
+      //let num = `this.phrase` + phraseNum;
+      //console.log(num);
+  
+      if (phraseNum == 1) {
+        console.log(this.phrase1);
+        
+      } else if (phraseNum == 2) {
+        console.log(this.phrase2);
+      }
+    }
+  
+    setLevel(newLevel) {
+      this.level = newLevel;
+    }
+}
+  
+//create
+let glados = new character("GLaDOS", "Oh, it's you.", "For science. You monster.");
+let wheatley = new character("Wheatley", "You may have a minor case of serious brain damage", "I am NOT a MORON!");
+
+//communicate
+glados.speak(1);
+glados.setLevel(5);
+wheatley.speak(2);
